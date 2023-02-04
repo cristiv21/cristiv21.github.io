7c68b36500dcb0a1749b165670d275601b72b4fd
cristiv21 <114905715+cristiv21@users.noreply.github.com>
2023-02-04 16:04:45 +0200

    Add files via upload

---
+++ framerusercontent.com/title.mjs
@@ -0,0 +1,10 @@
+var a=e=>({customHTMLHeadStart:`<!-- Google tag (gtag.js) -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-0QWB3T2130"><\/script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0QWB3T2130');
+<\/script>`,description:"Dusk Is a Stunning and Elegant Framer Portfolio Template Designed Specifically for Creatives, Designers, Freelancers, and Agency Owners To Help You Expand Your Brand.",favicon:new URL("https://framerusercontent.com/images/NwGjYyf6GehxGVNF0ZVxGnClEs.png").href,socialImage:new URL("https://framerusercontent.com/images/KKCvKc9Rfsxpgo5wG3JFOT3bVS4.jpg").href}),o=a;export{o as a};
+//# sourceMappingURL=chunk-GMQM6IUM.mjs.map
