3eb363fc1e4d47df4381aa6a17978f5f56e3d2d7
cristiv21 <114905715+cristiv21@users.noreply.github.com>
2023-02-04 16:10:24 +0200

    Add files via upload

---
+++ framerusercontent.com/chunk-GMQM6IUM.mjs
@@ -0,0 +1,10 @@
+var a=e=>({customHTMLHeadStart:`<!-- Google tag (gtag.js) -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-0QWB3T2130"><\/script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0QWB3T2130');
+<\/script>`,}),o=a;export{o as a};
+//# sourceMappingURL=chunk-GMQM6IUM.mjs.map
